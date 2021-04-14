c156739458349c00f0b27484bcf0731c4293f355
sdsweng <82483310+sdsweng@users.noreply.github.com>
2021-04-14 10:03:19 -0700

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,14 @@
 node default {
-  file { '/root/README' :
-    ensure  => file,
-    content => "This is a readme file",
-    owner   => "root",
-  }
+
 }
 
 node 'localhost.localdomain' {
   include role::master_server
+    file { '/root/README' :
+    ensure  => file,
+    content => $fqdn,
+    owner   => "root",
+  }
 }
 
 node /^web/ {
