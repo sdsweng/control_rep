90229cd36229b51efbb2821119fe911febc55122
sdsweng <82483310+sdsweng@users.noreply.github.com>
2021-04-14 11:44:10 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -4,22 +4,28 @@ class minecraft {
   }
   file { '/opt/minecraft/server.jar':
       ensure => file,
-      source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar'
+      source => 'https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar',
+      before => Service['minecraft'],
   }
+  
   package {'java':
     ensure => present,
   }
+  
   file {'/opt/minecraft/eula.txt':
     ensure => file,
     content => 'eula=true',
   }
+  
   file {'/etc/systemd/system/minecraft.service':
    ensure => file,
-   source => 'puppet:///modules/minecraft/minecraft.service'
+   source => 'puppet:///modules/minecraft/minecraft.service',
+   before => Service['minecraft'],
   }
   
   service {'minecraft':
      ensure => running,
      enable => true,
+     require => { Package['java],File['/opt/minecraft/eula.txt'],File['/etc/systemd/system/minecraft.service']}
   }
 }
