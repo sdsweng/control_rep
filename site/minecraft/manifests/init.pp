1e9548a1f9980c0dc617528b877c1b9026c70d61
sdsweng <82483310+sdsweng@users.noreply.github.com>
2021-04-14 12:56:52 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -23,9 +23,9 @@ class minecraft (
   
   file {'/etc/systemd/system/minecraft.service':
    ensure => file,
-   content => epp[ 'minecraft/minecraft.service', {
+   content => epp('minecraft/minecraft.service', {
        install_dir = $install_dir,
-   }
+   })
    before => Service['minecraft'],
   }
   
