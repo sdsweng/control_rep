214e373fb4bfe4ac2e46aaaca82753aebe9f8183
sdsweng <82483310+sdsweng@users.noreply.github.com>
2021-04-14 10:24:29 -0700

    Create ssh_server.pp

---
+++ site/profile/manifests/ssh_server.pp
@@ -0,0 +1,15 @@
+class profile::ssh_server {
+	package {'openssh-server':
+		ensure => present,
+	}
+	service { 'sshd':
+		ensure => 'running',
+		enable => 'true',
+	}
+	ssh_authorized_key { 'root@localhost.localdomain':
+		ensure => present,
+		user   => 'root',
+		type   => 'ssh-rsa',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDgMkp9G7ASrLvNBXbjBPAPB1MXiF/+H6h/JIJ1hnjOSjoAjpzv/PUb4YT5BF0R1NQCTwCmpIisThOQZDRsa2+lp/pqLZOyXeYoy5Skhzy6cakvxDs1b5cECsOH2Vw+XwxCqH/4XXYQCm5Z2uTpJ16AMCbGb7TG1/6AJWwKdWUbnTKKktf7s+L2m1WIX+Ah3GPNYMucMiQozOHGnCpmUwccZ6T9TS6X6I0HS7p8ajii5L18sOu9YH5JfnOoxWL0Sig9kv7xV6xs21wNUB+9H/8BKZ5FrwbDftZwez91+o7v6ftaQ7Lygi9rema5l8lBY2yBGEzol6NMXTzsofjGDcX1 r',
+	}  
+}
